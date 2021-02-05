656f9160267308ec375c1d24420f0cfaae3c49c6
ofurtumi <60275253+ofurtumi@users.noreply.github.com>
2021-02-05 15:49:10 +0000

    allskonar

---
+++ iframes/kubbur.js
@@ -0,0 +1,23 @@
+
+function setup() {
+  createCanvas(windowWidth,windowHeight,WEBGL);
+}
+
+function draw() {
+  background("black");
+  stroke("green")
+  noFill();
+  strokeWeight(3)
+  for (i = 0;i < 5;i++){
+    rotateX(frameCount * -0.01);
+    rotateY(frameCount * 0.01);
+    box(50);
+  }
+  
+  box(100);
+  rotateX(-1)
+  rotateY(-1)
+  box(150);
+  strokeWeight(1);
+  sphere(25);
+}
